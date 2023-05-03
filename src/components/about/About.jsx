27317e2ec76ed2e3d2180865c9545ad5edb65d18
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-05-03 16:15:43 +0530

    Project renovate

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import './about.scss'
 
-import Anshul from '../assets/ansu.jpeg'
+import Anshul from '../assets/crop.jpeg'
 import Male from '../assets/male.png'
-import Tex from '../assets/texts.png'
+import Tex from '../assets/ftex.png'
 const Project = () => {
   return (
     <div className='projects' id='about'>
@@ -16,9 +16,9 @@ const Project = () => {
         </span>
       </div>
       <div className="right">
-        <h1>About ME</h1>
+        <h2>About ME</h2>
         <h3>A Front-end Developer based in INDIA<LocationOnIcon className='icc'/></h3>
-        <p>Based in India, I am a front-end Web developer specializing in Javascript and React. I also Provide Clients With UI/UX Designs and Help Them with Their Design Strategies for Their Businesses. I can proudly say That I have a great understanding of the complete web development life cycle. I take pride in building top-notch websites and apps with elegant user interfaces. In my career, I have worked on a wide range of development verticals</p>
+        <p>Based in India, I am a front-end Web developer specializing in Javascript and React. I also Help Clients with Their Design Strategies for Their Businesses. I can proudly say That I have a great understanding of the complete web development life cycle. I take pride in building top-notch websites and apps with elegant user interfaces. In my career, I have worked on a wide range of development verticals</p>
 
         </div>
     </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -32,6 +32,7 @@ const Portfolio = () => {
         </div>
     </div>
      <PortItem/>
+     <PortItem/>
     </div>
   )
 }
